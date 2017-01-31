d8e8dd5c59e1a108842eb8eba54342cffd29a4df
myyusuf <myyusuf1911@gmail.com>
2017-01-31 17:41:44 +0700

    Fix edit window

--- src/base/EditWindow.js
+++ src/base/EditWindow.js
@@ -11,21 +11,32 @@ export default class EditWindow extends React.Component {
     componentDidMount() {
 
       let _this = this;
+
       this.refs.saveButton.on('click', (event) => {
         if(_this.props.onSave){
           _this.props.onSave();
         }
       });
+
+      this.refs.cancelButton.on('click', (event) => {
+        _this.refs.editWindow.close();
+      });
+
+      this.refs.deleteButton.on('click', (event) => {
+        if(_this.props.onDelete){
+          _this.props.onDelete();
+        }
+      });
     }
 
     open(){
-      this.refs.addWindow.open();
+      this.refs.editWindow.open();
     }
 
     render() {
       return (
-        <JqxWindow ref='addWindow'
-                      width={380} height={180} theme={'metro'} isModal={true}
+        <JqxWindow ref='editWindow'
+                      width={340} height={150} theme={'metro'} isModal={true}
                       resizable={false} autoOpen={false}>
           <div>
               <span>
@@ -36,12 +47,30 @@ export default class EditWindow extends React.Component {
             <table className="register-table">
               <tbody>
                 <tr>
-                    <td>{this.props.children}</td>
+                    <td colSpan={3}>{this.props.children}</td>
                 </tr>
                 <tr>
-                    <td>
+
+                  <td>
+                  <table style={{height: '100%', width: '100%'}}>
+                    <tbody>
+                    <tr>
+                      <td style={{width: '90%'}}>
+                      </td>
+                      <td style={{paddingRight: '20px'}}>
+                        <JqxButton width={60} height={25} ref='deleteButton' value='Delete' theme={'light'} template={'danger'}/>
+                      </td>
+                      <td>
+                        <JqxButton width={60} height={25} ref='cancelButton' value='Cancel' theme={'light'} template={'default'}/>
+                      </td>
+                      <td>
                         <JqxButton width={60} height={25} ref='saveButton' value='Save' theme={'light'} template={'success'}/>
-                    </td>
+                      </td>
+                    </tr>
+                    </tbody>
+                  </table>
+                  </td>
+
                 </tr>
               </tbody>
             </table>

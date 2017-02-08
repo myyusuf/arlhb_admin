429c855a40778c05a85c18fafe6284dbedbe6761
myyusuf <myyusuf1911@gmail.com>
2017-02-09 06:30:35 +0700

    Refactor back to basic

--- main.js
+++ main.js
@@ -1,4 +1,96 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './src/App';
-ReactDOM.render(<App />, document.getElementById('app'))
+import { getMenuData } from './app/base/ApplicationData';
+import NavigationBar from "./app/base/components/NavigationBar";
+import Menu from "./app/base/components/Menu";
+import Tabs from "./app/base/components/Tabs";
+import Button from "./app/base/components/Button";
+import RoleList from "./app/user/RoleList";
+
+var menuData = [
+  {
+      "id": "1",
+      "text": "Home",
+      "parentid": "-1",
+      "subMenuWidth": '250px'
+  },
+  {
+      "id": "2",
+      "text": "Customer Payment",
+      "parentid": "-1",
+      "subMenuWidth": '250px'
+  },
+  {
+      "id": "3",
+      "text": "Customer Transaction",
+      "parentid": "-1"
+  },
+  {
+      "id": "4",
+      "text": "Bank Transaction",
+      "parentid": "-1"
+  },
+  {
+      "id": "5",
+      "text": "Administration",
+      "parentid": "-1"
+  },
+  {
+      "id": "51",
+      "text": "Active Sessions",
+      "parentid": "5"
+  },
+  {
+      "id": "52",
+      "text": "Corporate Entity",
+      "parentid": "5"
+  },
+  {
+      "id": "53",
+      "text": "Country Zone",
+      "parentid": "5"
+  },
+  {
+      "id": "54",
+      "text": "Customer",
+      "parentid": "5"
+  },
+  {
+      "id": "55",
+      "text": "Holidays",
+      "parentid": "5"
+  },
+  {
+      "id": "56",
+      "text": "Location",
+      "parentid": "5"
+  },
+  {
+      "id": "57",
+      "text": "Operational Time",
+      "parentid": "5"
+  },
+  {
+      "id": "58",
+      "text": "Role",
+      "parentid": "5"
+  },
+  {
+      "id": "59",
+      "text": "User",
+      "parentid": "5"
+  },
+  {
+      "id": "6",
+      "text": "Report",
+      "parentid": "-1"
+  }
+];
+
+var roleList = new RoleList();
+var menu = new Menu({
+  data: menuData,
+  onClick: function(e){
+
+  }
+});
+
+menu.render($('#top-menu'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,19 @@
 var path = require('path');
 var webpack = require('webpack');
+
 module.exports = {
   entry: './main.js',
-  output: {
-    path: '/Users/myyusuf/Documents/Projects/Nusapro/Software/Server/arlhb/public/app/',
-    filename: 'bundle.js'
-  },
-  debug: true,
-  devtool: 'source-map',
-  devServer: {
-    inline: true,
-    port: 3333
-  },
+  // output: { path: __dirname, filename: 'bundle.js' },
+  output: { path: '/Users/myyusuf/Documents/Projects/Nusapro/Software/Server/arlhb/public/app/', filename: 'bundle.js' },
   module: {
     loaders: [
       {
-        test: /\.js$/,
-        // explicitely define the path to allow import of the jqwidget-react jsx files
-        include: [
-          path.resolve(__dirname, "src"),
-          path.resolve(__dirname, "main.js"),
-          path.resolve(__dirname, "node_modules/jqwidgets-framework/jqwidgets-react")
-        ],
-        // exclude: /node_modules/,
-        loader: 'babel',
+        loader: 'babel-loader',
+        exclude: /node_modules/,
         query: {
-          presets: ['es2015', 'react']
+          presets: ['es2015']
         }
       }
     ]
-  }
-}
+  },
+};

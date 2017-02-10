b5a6f5c69bf75ea14062983795031fbdb551c2c8
myyusuf <myyusuf1911@gmail.com>
2017-02-11 00:50:17 +0700

    Rest service

---
+++ app/base/service/RestService.js
@@ -0,0 +1,27 @@
+export default class RestService {
+
+  static post(options) {
+    $.ajax({
+          method: "POST",
+          url: options.url,
+          data: JSON.stringify(options.data),
+          beforeSend: function(xhr){
+            xhr.setRequestHeader('Accept', 'application/json');
+            xhr.setRequestHeader('Content-Type', 'application/json');
+          }
+        }).done(function() {
+            $("#successNotification").jqxNotification("open");
+            if(options.onSuccess){
+              options.onSuccess();
+            }
+        }).fail(function( jqXHR, textStatus, errorThrown) {
+            var errorMessage = 'Proses gagal. Status : ' + jqXHR.status + ' [' + jqXHR.statusText + '] : ' + jqXHR.responseText;
+            $("#errorNotification").html('<div>' + errorMessage + '</div>');
+            $("#errorNotification").jqxNotification("open");
+
+            if(options.onError){
+              options.onError();
+            }
+        });
+  }
+}
--- app/user/AddRoleWindow.js
+++ app/user/AddRoleWindow.js
@@ -2,6 +2,7 @@ import Button from '../base/components/Button';
 import Form from '../base/components/Form';
 import AddWindow from '../base/components/AddWindow';
 import Component from '../base/components/Component';
+import RestService from '../base/service/RestService';
 import RoleForm from './RoleForm';
 
 export default class AddRoleWindow extends Component{
@@ -14,25 +15,10 @@ export default class AddRoleWindow extends Component{
 
     var roleForm = new RoleForm({
       onValidationSuccess: function(formValue){
-        $.ajax({
-              method: "POST",
-              url: "/roles",
-              data: JSON.stringify(formValue),
-              beforeSend: function(xhr){
-                xhr.setRequestHeader('Accept', 'application/json');
-                xhr.setRequestHeader('Content-Type', 'application/json');
-              }
-            }).done(function() {
-                $("#successNotification").jqxNotification("open");
-                // _this.window.close();
-                if(_this.onSaveSuccess){
-                  _this.onSaveSuccess();
-                }
-            }).fail(function( jqXHR, textStatus, errorThrown) {
-                var errorMessage = 'Proses gagal. Status : ' + jqXHR.status + ' [' + jqXHR.statusText + '] : ' + jqXHR.responseText;
-                $("#errorNotification").html('<div>' + errorMessage + '</div>');
-                $("#errorNotification").jqxNotification("open");
-            });
+        RestService.post({
+          url: '/roles',
+          data: formValue
+        });
       }
     });
 
